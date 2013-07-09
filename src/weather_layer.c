d4986c3074d82c980bc441edaed3e7872b3db628
Michael D. Smith <michael.smith.erdc@gmail.com>
2013-07-09 06:37:01 -0400

    Update weather_layer.c

--- src/weather_layer.c
+++ src/weather_layer.c
@@ -55,6 +55,22 @@ void weather_layer_set_temperature(WeatherLayer* weather_layer, int16_t t) {
 	text_layer_set_text(&weather_layer->temp_layer, weather_layer->temp_str);
 }
 
+void weather_layer.set_forecast(WeatherLayer* weather_layer, int16_t hi, int16_t lo) {
+	memcpy(fcstlow_text, itoa(fcstlow_tuple->value->int16), fcstlow_tuple->length);
+	memcpy(fcsthigh_text, itoa(fcsthigh_tuple->value->int16), fcsthigh_tuple->length);
+
+	//Tuple* fcstcond_tuple = dict_find(received, WEATHER_KEY_FCST);
+	//memcpy(fcstcond_text, fcstcond_tuple->value->cstring, strlen(fcstcond_tuple->value->cstring));
+	//fcstcond_text[strlen(fcstcond_tuple->value->cstring)] = '\0';
+
+	strcat(fcst_text, fcstlow_text);
+	strcat(fcst_text, "° / ");
+	strcat(fcst_text, fcsthigh_text);
+	strcat(fcst_text, "°  ");
+	//strcat(fcst_text, fcstcond_text);
+	text_layer_set_text(&text_fcst_layer, fcst_text);
+}
+
 void weather_layer_deinit(WeatherLayer* weather_layer) {
 	if(weather_layer->has_weather_icon)
 		bmp_deinit_container(&weather_layer->icon_layer);
